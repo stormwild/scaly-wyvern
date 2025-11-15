693e24dbf4ee098ecad5ca7c4d8e06e6b8c63fad
Alexander R Torrijos <artorrijos@gmail.com>
2025-11-15 12:52:59 +0800

    Add support for multi-page builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
+/// <reference types="node" />
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   base: '/scaly-wyvern/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(rootDir, 'index.html'),
+        grid: resolve(rootDir, 'grid.html'),
+      },
+    },
+  },
 })
